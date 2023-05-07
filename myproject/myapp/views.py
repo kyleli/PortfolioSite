6c22460dc2b2d7adcb62f6fb31f172409409f2a4
Kyle Li <kyleseanli@gmail.com>
2023-05-07 09:40:35 -0400

    New Html Template Test

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello Lucy!</h1> <body>Hello<body>')
+    return render(request, "index.html")
