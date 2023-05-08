416c60afbddfd68546c24cb05e011e9df8452b8c
Kyle Li <kyleseanli@gmail.com>
2023-05-08 12:39:29 -0400

    Testing changes

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    name = "Kyle"
+    return render(request, "index.html", {'name': name})
