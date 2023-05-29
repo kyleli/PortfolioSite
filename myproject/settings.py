0b52f7495968bb72d2eb1c130d79d77182d826e6
Kyle Li <kyleseanli@gmail.com>
2023-05-29 19:58:53 -0400

    Removed references to Config

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from config import SECRET_KEY
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
